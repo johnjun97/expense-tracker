22d68382ff52e08c20b83ca3cab80a0ccd777e0c
johnjun <johnjun97@outlook.com>
2026-08-28 15:37:02 +0800

    add PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: 'prompt',
+
+      manifest: {
+        name: 'Expense Tracker',
+        short_name: 'Expenses',
+        description: 'Expense Tracker',
+        start_url: '/',
+        display: 'standalone',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+      },
+    }),
+  ],
 })
